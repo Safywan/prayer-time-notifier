0ac6a276ebf53f5a95cec7617757608b3cf93fb0
Safwan <155366731+Safywan@users.noreply.github.com>
2026-02-03 21:59:32 +1100

    Tested out python schedule library. Notification works in time

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-import scraper
-
+import scraper 
+import time
+import datetime
+import notification
+import schedule
+from dotenv import load_dotenv
+load_dotenv()
 url = "https://awqat.com.au/mgm/"
 
 def main():
     prayer_times = scraper.fetch_prayer_times(url)
-    print(prayer_times)
+    #print(prayer_times)
     
+    # Get the time right now
+    now = datetime.datetime.now()
+    curr_time = now.strftime("%-I:%M%p")
+    
+    if curr_time in prayer_times:
+    # Send notification if the current time is in the prayer times
+        notification.prayer_time_notification(prayer_times, curr_time)
+        print("Notification successful!")
 
-if __name__ == "__main__":
-    main()    
+schedule.every(10).seconds.do(main)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+       
